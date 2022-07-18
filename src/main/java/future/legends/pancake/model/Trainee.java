4603982bfae8405d408ef981615676b336316d05
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-07-18 15:12:26 +0100

    Trainee & Trainee Factory

---
+++ src/main/java/future/legends/pancake/model/Trainee.java
@@ -0,0 +1,26 @@
+package future.legends.pancake.model;
+
+public class Trainee {
+    // Place in Trainee Parameters Here
+    // private _______
+    // private _______
+    // private _______
+
+
+//    public Trainee(```params here```) {
+//        this.paramName = paramName;
+//    }
+
+    public Trainee() {
+
+    }
+
+    /*
+
+
+    Getters & Setters
+
+
+
+     */
+}
