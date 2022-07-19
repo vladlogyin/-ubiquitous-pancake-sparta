d0d320578c24e01f8536c430de408dc66af11eac
OTDZ <omartehami6@gmail.com>
2022-07-19 13:52:00 +0100

    Implemented getSimulationOutputFrequency function

--- src/main/java/future/legends/pancake/viewer/MainViewer.java
+++ src/main/java/future/legends/pancake/viewer/MainViewer.java
@@ -35,6 +35,37 @@ public class MainViewer {
 
     }
 
+    public int getSimulationOutputFrequency(){
+
+        boolean validInput = false;
+        int frequencyChoice = 0;
+
+        while (!validInput){
+
+            System.out.println("When would you like to receive simulation information? Select a number: ");
+            System.out.println("1. End of simulation");
+            System.out.println("2. End of every month");
+
+            try{
+                frequencyChoice = scanner.nextInt();
+                if (frequencyChoice == 1 || frequencyChoice == 2){
+                    validInput = true;
+                }
+                else{
+                    throw new InputMismatchException();
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("\nInvalid input: Please enter 1 or 2\n");
+                scanner.nextLine();
+            }
+
+        }
+
+        return frequencyChoice;
+
+    }
+
     public void displaySimulationResults(int openCentres, int fullCentres, int activeTrainees, int waitingTrainees){
 
         System.out.println("Number of open centres: " + openCentres);
