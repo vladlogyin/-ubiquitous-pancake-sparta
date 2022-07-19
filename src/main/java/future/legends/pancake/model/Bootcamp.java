ac28eb0505aa27402ddb5cead6ecc9f03f5b2c27
Baaartosz <bartosz_p@hotmail.com>
2022-07-19 15:16:55 +0100

    Implemented Bootcamp

---
+++ src/main/java/future/legends/pancake/model/Bootcamp.java
@@ -0,0 +1,30 @@
+package future.legends.pancake.model;
+
+import java.util.Collection;
+import java.util.Queue;
+
+public class Bootcamp extends TraineeCentre{
+    Bootcamp() {
+        capacity = 500;
+    }
+    int numberOfLowAttendanceMonths = 0;
+
+    @Override
+    public Collection<Trainee> getEnrolledTrainees() {
+        return super.getEnrolledTrainees();
+    }
+
+    @Override
+    void enrollTrainees(Queue<Trainee> waitingList) {
+        // TODO maybe instead of accepting 1-50 random employees implement a method that
+        //      takes on as many trainees as possible.
+        int amountToEnroll = getAvailableSpots();
+        if (waitingList.isEmpty()) {
+            return;
+        }
+        if (waitingList.size() < amountToEnroll) {
+            amountToEnroll = waitingList.size();
+        }
+        enrollTrainees(waitingList, amountToEnroll);
+    }
+}
