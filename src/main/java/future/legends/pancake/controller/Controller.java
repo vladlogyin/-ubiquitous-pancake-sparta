80257d2246161f006a19d82eccdab82bf05086ff
ykolarov <kolarov01@protonmail.me>
2022-07-19 13:40:13 +0100

    Implement MVC controller

--- src/main/java/future/legends/pancake/controller/Controller.java
+++ src/main/java/future/legends/pancake/controller/Controller.java
@@ -1,2 +1,14 @@
-package future.legends.pancake.controller;public class Controller {
+package future.legends.pancake.controller;
+
+import future.legends.pancake.model.Simulator;
+import future.legends.pancake.viewer.MainViewer;
+
+public class Controller {
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+        MainViewer mainViewer = new MainViewer();
+
+        simulator.simulateMonths(mainViewer.getSimulationDuration());
+        //mainViewer.displaySimulationResults();
+    }
 }
