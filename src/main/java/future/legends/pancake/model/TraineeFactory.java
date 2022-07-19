d1c687777a0ba12ddc42e653418917cdabf5839c
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-07-19 15:13:24 +0100

    TraineeFactory Update - random assignment of courses to Trainees

--- src/main/java/future/legends/pancake/model/TraineeFactory.java
+++ src/main/java/future/legends/pancake/model/TraineeFactory.java
@@ -13,7 +13,9 @@ public class TraineeFactory {
     public static Collection <Trainee> generateTrainees(int numOfTrainees) {
         Collection<Trainee> traineeCollection = new ArrayList<Trainee>(numOfTrainees);
         for (int i = 0; i < numOfTrainees; i++) {
-            traineeCollection.add(new Trainee());
+            TraineeCourse[] courses = TraineeCourse.values();
+            int selectEnum = new Random().nextInt(courses.length);
+            traineeCollection.add(new Trainee(courses[selectEnum]));
         }
         return traineeCollection;
     }
