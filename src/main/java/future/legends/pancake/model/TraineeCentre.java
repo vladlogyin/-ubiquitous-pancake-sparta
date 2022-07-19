f841ad6cf321b691cf905aca0f98beb6c0772df6
Baaartosz <bartosz_p@hotmail.com>
2022-07-19 14:20:01 +0100

    Refactor TraineeCentre into an abstract class

--- src/main/java/future/legends/pancake/model/TraineeCentre.java
+++ src/main/java/future/legends/pancake/model/TraineeCentre.java
@@ -2,15 +2,19 @@ package future.legends.pancake.model;
 
 import java.util.*;
 
-public class TraineeCentre {
-    private final int CAPACITY = 100;
-    private Collection<Trainee> enrolledTrainees = new ArrayList<>();
+public abstract class TraineeCentre {
+    int capacity;
+    Collection<Trainee> enrolledTrainees = new ArrayList<>();
 
     public int getNumberOfEnrolledTrainees(){
         return enrolledTrainees.size();
     }
 
-    public void enrollTrainees(Queue<Trainee> waitingList){
+    public Collection<Trainee> getEnrolledTrainees(){
+        return enrolledTrainees;
+    }
+
+    void enrollTrainees(Queue<Trainee> waitingList){
         Random r = new Random();
         int amountToEnroll = r.nextInt((50)) + 1;
 
@@ -21,13 +25,17 @@ public class TraineeCentre {
         enrollTrainees(waitingList, amountToEnroll);
     }
 
-    private void enrollTrainees(Queue<Trainee> waitingList, int toEnroll){
+    protected void enrollTrainees(Queue<Trainee> waitingList, int toEnroll){
         for (int i = 0; i < toEnroll; i++) {
             enrolledTrainees.add(waitingList.remove());
         }
     }
 
     public int getAvailableSpots(){
-        return CAPACITY - getNumberOfEnrolledTrainees();
+        return capacity - getNumberOfEnrolledTrainees();
+    }
+
+    public int getCapacity(){
+        return capacity;
     }
 }
