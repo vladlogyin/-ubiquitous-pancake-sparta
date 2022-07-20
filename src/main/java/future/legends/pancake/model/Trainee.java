9e60c5079bcad52cd563668c08d19a66cf3dc6ca
Vlad Logyin <vladlogyin@gmail.com>
2022-07-20 11:10:00 +0100

    Keep track of months passed in training

--- src/main/java/future/legends/pancake/model/Trainee.java
+++ src/main/java/future/legends/pancake/model/Trainee.java
@@ -2,9 +2,12 @@ package future.legends.pancake.model;
 
 public class Trainee {
     private TraineeCourse courseType;
+    private int monthsPassed;
+
 
     public Trainee(TraineeCourse courseType) {
         this.courseType = courseType;
+        monthsPassed=0;
     }
 
     public Trainee() {
@@ -18,4 +21,14 @@ public class Trainee {
     public void setCourseType(TraineeCourse courseType) {
         this.courseType = courseType;
     }
+
+    public void monthPassed()
+    {
+        monthsPassed++;
+    }
+    public boolean hasGraduated()
+    {
+        return monthsPassed>3;
+    }
+
 }
