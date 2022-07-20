82f5dd0c1ed2e9c0e3b5a61626581b2c2dab273c
Vlad Logyin <vladlogyin@gmail.com>
2022-07-20 08:29:13 +0100

    Add limit information to the CentreType enum

--- src/main/java/future/legends/pancake/model/CentreType.java
+++ src/main/java/future/legends/pancake/model/CentreType.java
@@ -1,7 +1,16 @@
 package future.legends.pancake.model;
 
 public enum CentreType {
-    TRAINING_HUB,
-    BOOTCAMP,
-    TECH_CENTRE
+    TRAINING_HUB(TrainingHub.class,-1);
+    Class<? extends TraineeCentre> type;
+    /**
+     * Limit of how many centres of this type can be opened
+     * set to -1 for an unlimited amount
+     */
+    int limit;
+
+    CentreType(Class<TrainingHub> t, int limit) {
+        this.type = t;
+        this.limit=limit;
+    }
 }
