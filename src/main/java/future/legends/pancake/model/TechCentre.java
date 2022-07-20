c25432df5e0e29147650ac38fe6e354175fc5977
Baaartosz <bartosz_p@hotmail.com>
2022-07-20 09:19:31 +0100

    Implemented random course type in Tech Centre constructor.

--- src/main/java/future/legends/pancake/model/TechCentre.java
+++ src/main/java/future/legends/pancake/model/TechCentre.java
@@ -1,7 +1,18 @@
 package future.legends.pancake.model;
 
+import java.util.Random;
+
 public class TechCentre extends TraineeCentre{
+
+    private TraineeCourse course;
+
+    public TraineeCourse getCourseType() {
+        return course;
+    }
+
     TechCentre(){
+        Random r = new Random();
+        course = TraineeCourse.values()[r.nextInt(TraineeCourse.values().length)];// TODO - assign course on creation.
         capacity = 200;
     }
 
