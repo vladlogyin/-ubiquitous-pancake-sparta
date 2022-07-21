51b0238fe43dfd0d748e0993ca1bd2a8039fd9bd
OTDZ <omartehami6@gmail.com>
2022-07-21 14:36:01 +0100

    Added info logs

--- src/main/java/future/legends/pancake/model/TraineeFactory.java
+++ src/main/java/future/legends/pancake/model/TraineeFactory.java
@@ -1,5 +1,7 @@
 package future.legends.pancake.model;
 
+import future.legends.pancake.logger.Logger;
+
 import java.util.*;
 
 public class TraineeFactory {
@@ -17,6 +19,7 @@ public class TraineeFactory {
             int selectEnum = new Random().nextInt(courses.length);
             traineeCollection.add(new Trainee(courses[selectEnum]));
         }
+        Logger.info("Generated " + numOfTrainees + " new trainees");
         return traineeCollection;
     }
 
