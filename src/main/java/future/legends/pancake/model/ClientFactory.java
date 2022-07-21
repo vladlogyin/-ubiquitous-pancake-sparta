d28d38690fba98ce33f7a3d337fc7392dd43c0d6
Vlad Logyin <vladlogyin@gmail.com>
2022-07-21 15:20:36 +0100

    Make ClientFactory fully static

--- src/main/java/future/legends/pancake/model/ClientFactory.java
+++ src/main/java/future/legends/pancake/model/ClientFactory.java
@@ -7,38 +7,11 @@ import java.util.Random;
 
 public class ClientFactory {
 
-    private static Collection<Client> allClients;
-    private static Collection<Client> happyClients;
-
-    public ClientFactory() {
-        allClients = new ArrayList<>();
-    }
-
-    public void create(){
+    public static Client create(){
         Random random = new Random();
         int randomRequirement = new Random().nextInt(15,31);
         TraineeCourse[] courses = TraineeCourse.values();
         int selectEnum = random.nextInt(courses.length);
-        allClients.add(new Client(randomRequirement, courses[selectEnum]));
+        return new Client(randomRequirement, courses[selectEnum]);
     }
-
-    public static Collection<Client> getAllClients() {
-        return allClients;
-    }
-
-    public static void pauseHappyClient(Client c){
-        happyClients.add(c);
-    }
-
-    public void checkIfHappyClientIsBack() {
-        Iterator<Client> i = happyClients.iterator();
-        while(i.hasNext()){
-            Client c = i.next();
-            if(c.getMonthsPassed() % 12 == 0) {
-                allClients.add(c); // add client to original list
-                i.remove(); // remove client from paused happy clients
-            }
-        }
-    }
-
 }
