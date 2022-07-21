c2494bc708c16ab7f5c72d3beeded0ed2650ae52
OTDZ <omartehami6@gmail.com>
2022-07-21 15:05:18 +0100

    Added error logs

--- src/main/java/future/legends/pancake/model/QueueProvider.java
+++ src/main/java/future/legends/pancake/model/QueueProvider.java
@@ -136,6 +136,7 @@ public class QueueProvider {
         }
         catch (NullPointerException e)
         {
+            Logger.error("Paused trainee queue and new trainee queue are both empty " + e.getMessage());
             throw new NoSuchElementException(e);
         }
     }
